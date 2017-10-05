5e3f60b5f534381df644d7b2fc6e09f056ffb352
Dominique LaSalle <dominique@solidlake.com>
2017-10-05 14:31:37 -0500

    Add domtest

---
+++ src/test/DomTest.hpp
@@ -0,0 +1,154 @@
+/**
+ * @file DomTest.hpp
+ * @brief Top level header for DomTest 
+ * @author Dominique LaSalle <wildriver@domnet.org>
+ * Copyright 2015
+ * @version 1
+ * @date 2015-08-22
+ */
+
+
+
+
+#ifndef DOMTEST_HPP
+#define DOMTEST_HPP
+
+
+
+
+#include <stdexcept>
+#include <exception>
+#include <string>
+#include <iostream>
+
+
+
+
+/******************************************************************************
+* MACROS **********************************************************************
+******************************************************************************/
+
+
+#define testEquals(a,b) \
+  if ((a) != (b)) { \
+    std::cerr << "Test Failed: " << #a << ":'" << a << "' != " << #b << ":'" \
+        << b << "' at " << __LINE__ << std::endl; \
+    throw DomTest::TestFailed("Equals Failed"); \
+  }
+
+
+#define testStringEquals(a,b) \
+  if ((a).compare(b) != 0) { \
+    std::cerr << "Test Failed: " << #a << ":'" << a << "' != " << #b << ":'" \
+        << b << "' at " << __LINE__ << std::endl; \
+    throw DomTest::TestFailed("Equals Failed"); \
+  }
+
+
+#define testTrue(a) \
+  if (!(a)) { \
+    std::cerr << "Test Failed: '" << #a << "' is false at: " << __LINE__ \
+      << std::endl; \
+    throw DomTest::TestFailed("True Failed"); \
+  }
+
+
+#define testGreaterThan(a,b) \
+  if ((a) <= (b)) { \
+    std::cerr << "Test Failed:" << #a << ":'" << a << "' <= " << #b << ":'" \
+        << b << "' at " << __LINE__ << std::endl; \
+    throw DomTest::TestFailed("Greater Than Failed"); \
+  }
+
+
+#define testGreaterThanOrEqual(a,b) \
+  if ((a) < (b)) { \
+    std::cerr << "Test Failed:" << #a << ":'" << a << "' < " << #b << ":'" \
+        << b << "' at " << __LINE__ << std::endl; \
+    throw DomTest::TestFailed("Greater Than Or Equal Failed"); \
+  }
+
+
+#define UNITTEST(CLASS, TEST) \
+  void _unittest_ ## CLASS ## _ ## TEST ## _func(void); \
+  bool _unittest_ ## CLASS ## _ ## TEST ## _result = \
+      DomTest::UnitTest( \
+          __FILE__":"#CLASS"->"#TEST, \
+          _unittest_ ## CLASS ## _ ## TEST ## _func); \
+  void _unittest_ ## CLASS ## _ ## TEST ## _func(void) \
+
+
+
+namespace DomTest
+{
+
+volatile int numberFailed = 0;
+volatile int numberPassed = 0;
+
+class TestFailed : public std::logic_error
+{
+  public:
+    TestFailed(std::string const & str) :
+        std::logic_error(str)
+    {
+    }
+};
+
+bool UnitTest(
+    char const * name,
+    void (*test)(void))
+{
+  try {
+    test();
+    std::cerr << "Test " << name << " PASSED." << std::endl;
+    ++numberPassed;
+    return true;
+  } catch (TestFailed const & e) {
+    std::cerr << "Test " << name << " FAILED." << std::endl;
+    ++numberFailed;
+    return false;
+  }
+}
+
+
+
+}
+
+/******************************************************************************
+* MAIN ************************************************************************
+******************************************************************************/
+
+
+int main(
+    int argc,
+    char ** argv)
+{
+  // make sure we don't have any useless arguments
+  if (argc > 1) {
+    for (int i=1; i<argc; ++i) {
+      std::cerr << "Unused parameter: " << argv[i] << std::endl;
+    }
+    return 0;
+  }
+
+
+
+  if (DomTest::numberPassed == 0 && DomTest::numberFailed == 0) {
+    std::cerr << "No tests run." << std::endl;
+    return 0;
+  }
+
+  std::cout << DomTest::numberPassed << " tests passed." << std::endl;
+  std::cout << DomTest::numberFailed << " tests failed." << std::endl;
+  
+  if (DomTest::numberFailed > 0) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
+
+
+
+#endif
