b96642f3ddb5ca700f962d83d3481d3ef2a6af6d
Dominique LaSalle <dominique@solidlake.com>
2017-10-12 17:39:48 -0500

    Add header for SubgraphExtractor unit test

---
+++ src/test/SubgraphExtractor_test.cpp
@@ -0,0 +1,26 @@
+/**
+* @file SubgraphExtractor_test.cpp
+* @brief Unit tests for the SubgraphExtractor class.
+* @author Dominique LaSalle <dominique@solidlake.com>
+* Copyright 2017
+* @version 1
+* @date 2017-10-12
+*/
+
+
+
+#include "DomTest.hpp"
+#include "SubgraphExtractor.hpp"
+
+
+namespace dolos
+{
+
+UNITTEST(SubgraphExtract, Partitions)
+{
+
+
+
+}
+
+}
