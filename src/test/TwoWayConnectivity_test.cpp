f4a1705b637e0b3f9f580540c6320f1264c3764e
Dominique LaSalle <dominique@solidlake.com>
2017-10-20 08:21:36 -0500

    Start unit tests for twoway connectivity

---
+++ src/test/TwoWayConnectivity_test.cpp
@@ -0,0 +1,25 @@
+/**
+* @file TwoWayConnectivity_test.cpp
+* @brief Unit tests for the TwoWayConnectivity class.
+* @author Dominique LaSalle <dominique@solidlake.com>
+* Copyright 2017
+* @version 1
+* @date 2017-10-20
+*/
+
+
+
+#include "DomTest.hpp"
+#include "TwoWayConnectivity.hpp"
+
+
+
+namespace
+{
+
+UNITTEST(TwoWayConnectivity, )
+{
+}
+
+
+}
