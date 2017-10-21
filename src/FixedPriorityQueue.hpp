8b6f70802af3d6f3127ac5b27d7bdf7791a37df1
Dominique LaSalle <dominique@solidlake.com>
2017-10-20 20:19:31 -0500

    Checkpotni pq implementation

--- src/FixedPriorityQueue.hpp
+++ src/FixedPriorityQueue.hpp
@@ -35,8 +35,6 @@ class FixedPriorityQueue
       m_size(0)
     {
       ASSERT_LESS(min, max);
-
-      
     }
 
 
@@ -77,6 +75,43 @@ class FixedPriorityQueue
       m_data[index].value = value;
 
       place(index);
+    }
+
+
+    /**
+    * @brief Update the key associated with a given value.
+    *
+    * @param key The new key for the value.
+    * @param value The value.
+    */
+    void update(
+        K const key,
+        V const value) noexcept
+    {
+      ASSERT_LESS(static_cast<size_t>(value), m_index.size());
+      ASSERT_NOTEQUAL(m_index[value], NULL_INDEX);
+
+      size_t const index = m_index[value]; 
+      m_data[index].key = key;
+
+      // update position
+    }
+
+
+    /**
+    * @brief Get the key associated with the given value.
+    *
+    * @param value The value.
+    *
+    * @return The key.
+    */
+    K get(
+        V const value) noexcept
+    {
+      ASSERT_LESS(static_cast<size_t>(value), m_index.size());
+      ASSERT_NOTEQUAL(m_index[value], NULL_INDEX);
+
+      return m_data[m_index[value]].data;
     }
 
 
@@ -122,26 +157,100 @@ class FixedPriorityQueue
 
 
     /**
+    * @brief Get the index of the parent.
+    *
+    * @param index
+    *
+    * @return 
+    */
+    size_t parentIndex(
+        size_t const index) const noexcept
+    {
+      return index / 2;
+    }
+
+
+    /**
+    * @brief Get the index of the left child node.
+    *
+    * @param index The current index.
+    *
+    * @return The left child index.
+    */
+    size_t leftChildIndex(
+        size_t const index) const noexcept
+    {
+      return (index * 2)+1;
+    }
+
+
+    /**
+    * @brief Get the index of the right child node.
+    *
+    * @param index The current index.
+    *
+    * @return The right child index.
+    */
+    size_t rightChildIndex(
+        size_t const index) const noexcept
+    {
+      return leftChildIndex(index) + 1;
+    }
+
+
+
+    /**
     * @brief Fill a whole created in the heap.
     *
     * @param index The index of the hole to fill.
     */
     void fill(
-        size_t const index)
+        size_t const index) noexcept
     {
+      // what we'll do is move the bottom node to this position
+      m_data[index] = m_data[--m_size];
+      m_index[m_data[index]] = index;
+
+      siftDown(index); 
     }
 
 
     /**
-    * @brief Correctly float an item up into the heap that was placed at the
-    * bottom.
+    * @brief Correctly float an item up into the heap.
     *
     * @param index The index of the value to place.
     */
-    void place(
-        size_t const index)
+    void siftUp(
+        size_t index) noexcept
     {
+      while (index > 0) {
+        size_t const parent = parentIndex(index);
+        if (m_data[parent].key >= m_data[index].key) {
+          // reached a valid state
+          break;
+        }
+        V const currentValue = m_data[index].value;
+        V const parentValue = m_data[parent].value; 
+        std::swap(m_index[currentValue], m_index[parentValue]);
+        std::swap(m_data[index], m_data[parentIndex]);
+        index = parent;
+      }
     }
+
+    /**
+    * @brief Correctly sink an item up into the heap.
+    *
+    * @param index The index of the value to place.
+    */
+    void siftDown(
+        size_t const index) noexcept
+    {
+      while (index < m_size) {
+        size_t const leftIndex = left
+        
+      }
+    }
+
 };
 
 
