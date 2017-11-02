092eebf8653b33e7d468d36a1d118281fac7c31b
Dominique LaSalle <dominique@solidlake.com>
2017-11-01 23:40:12 -0500

    Blah

--- src/FMRefiner.cpp
+++ src/FMRefiner.cpp
@@ -28,63 +28,38 @@ namespace
 {
 
 
-struct refinement_info_struct {
-  sl::FixedPriorityQueue<wgt_diff_type, vtx_type> pqs[NUM_BISECTION_PARTS];
-  PartitioningAnalyzer analyzer;
-  sl::BitArray visited;
-};
-
-void performIter(
-    ConstantGraph const * const graph,
-    Partitioning * const partitioning,
-    TwoWayConnectivity * const connectivity,
-    refinement_info_struct * const info)
+void pickSide(
+    PartitioningAnalyzer const * const analyzer,
+    FixedPriorityQueue const * const pqs)
 {
-  // fill priority queue with boundary vertices
-  for (pid_type side = 0; side < NUM_BISECTION_PARTS; ++side) {
-    vtx_type const numVertices = connectivity->getNumBorderVertices(side);
-    for (vtx_type v = 0; v < numVertices; ++v) {
-      info->pqs[side].add(-connectivity->getVertexDelta(v), v);
-    }
-  }
-
-  // empty priority queues
-  while (info->pqs[0].size() > 0 || info->pqs[1].size() > 0) {
-
-    pid_type from;
-    // decide our direction of movement
-    if (info->analyzer.isOverWeight(0)) {
-      // fix balance by moving vertices out of 0
-      from = 0;
-    } else if (info->analyzer.isOverWeight(1)) {
-      // fix balance by moving vertices out of 1
+  pid_type from;
+  // decide our direction of movement
+  if (analyzer.isOverWeight(0)) {
+    // fix balance by moving vertices out of 0
+    from = 0;
+  } else if (analyzer.isOverWeight(1)) {
+    // fix balance by moving vertices out of 1
+    from = 1;
+  } else {
+    // if a priority queue is empty choose the other one
+    if (pqs[0].size() == 0) {
       from = 1;
-    } else {
-      // if a priority queue is empty choose the other one
-      if (info->pqs[0].size() == 0) {
-        from = 1;
-      } else if (info->pqs[1].size() == 0) {
+    } else if (pqs[1].size() == 0) {
+      from = 0;
+    } else { 
+      // move from highest priority pq
+      if (pqs[0].max() > pqs[1].max()) {
         from = 0;
-      } else { 
-        // move from highest priority pq
-        if (info->pqs[0].max() > info->pqs[1].max()) {
-          from = 0;
-        } else if (info->pqs[0].max() > info->pqs[1].max()) {
-          from = 1;
-        } else {
-          // break the tie via a random number
-          from = sl::Random::inRange(0,1);
-        }
+      } else if (pqs[0].max() > pqs[1].max()) {
+        from = 1;
+      } else {
+        // break the tie via a random number
+        from = sl::Random::inRange(0,1);
       }
     }
-    pid_type const to = from ^ 0x01;
-
-    wgt_diff_type const gain = info->pqs[from].max(); 
-    vtx_type const vertex = info->pqs[from].pop();
-
-    moveVertex();
-
   }
+
+  return from;
 }
 
 
@@ -114,17 +89,57 @@ void FMRefiner::refine(
     Partitioning * const partitioning,
     ConstantGraph const * const graph) const
 {
-  refinement_info_struct info{
-    { {0, graph->getNumVertices()}, {0, graph->getNumVertices()} },
-    {partitioning, target},
-    {graph->getNumVertices()}
+  FixedPriorityQueue[] pqs{ \
+    {0, graph->getNumVertices()}, \
+    {0, graph->getNumVertices()} \
   };
+  PartitioningAnalyzer analyzer(partitioning, target);
+  BitArray visited(graph->getNumVertices());
+
+  std::vector<vtx_type> moves;
+  moves.reserve(graph->getNumVertices());
 
   int const maxRefIters = params->getMaxRefinementIterations();
   for (int refIter = 0; refIter < maxRefIters; ++refIter) {
      
-    performIter(graph, partitioning, connectivity, &info);
+    // fill priority queue with boundary vertices
+    for (pid_type side = 0; side < NUM_BISECTION_PARTS; ++side) {
+      vtx_type const numVertices = connectivity->getNumBorderVertices(side);
+      for (vtx_type v = 0; v < numVertices; ++v) {
+        pqs[side].add(-connectivity->getVertexDelta(v), v);
+      }
+    }
 
+    moves.clear();
+    wgt_type bestCut = partitioning->getCutEdgeWeight();
+
+    // move all possible vertices
+    while (pqs[0].size() > 0 || pqs[1].size() > 0) {
+      pid_type const from = pickSide(&analyzer, pqs); 
+      pid_type const to = from ^ 1;
+
+      wgt_diff_type const gain = pqs[from].max();
+      vtx_type const vertex = pqs[from].pop();
+
+      // update connectivity
+      connectivity.move(vertex);
+
+      moves.emplace_back(vertex); 
+
+      visited[vertex] = true;
+    }
+
+    // undo bad moves
+    for (size_t i = moves.size(); i > 0;) {
+      --i;
+
+      vtx_type const vertex = moves[i];
+
+      connectivity.move(vertex);
+    }
+
+
+    // empty priority queues
     if (refIter+1 < maxRefIters) {
       // we'll do another loop
       visited.clear();
