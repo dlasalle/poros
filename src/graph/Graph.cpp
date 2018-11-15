398f6e98660a0e5449ac9747829317394d9d35f4
Dominique LaSalle <dominique@solidlake.com>
2018-11-14 22:27:48 -0800

    Add Graph class

---
+++ src/graph/Graph.cpp
@@ -0,0 +1,166 @@
+/**
+* @file Graph.cpp
+* @brief The implementation of the Graph class.
+* @author Dominique LaSalle <dominique@solidlake.com>
+* Copyright 2017-2018
+* @version 1
+* @date 2017-10-09
+*/
+
+
+
+#include "Base.hpp"
+#include "graph/Graph.hpp"
+#include "solidutils/Debug.hpp"
+#include "solidutils/Array.hpp"
+
+
+namespace dolos
+{
+
+
+/******************************************************************************
+* CONSTRUCTORS / DESTRUCTOR ***************************************************
+******************************************************************************/
+
+
+Graph::Graph(
+    vtx_type numVertices,
+    adj_type numEdges,
+    adj_type const * const edgePrefix,
+    vtx_type const * const edgeList,
+    wgt_type const * const vertexWeight,
+    wgt_type const * const edgeWeight) :
+  m_uniformEdgeWeight(true),
+  m_uniformVertexWeight(true),
+  m_numVertices(numVertices),
+  m_numEdges(numEdges),
+  m_totalVertexWeight(0),
+  m_totalEdgeWeight(0),
+  m_edgePrefix(edgePrefix, numVertices+1),
+  m_edgeList(edgeList, numEdges),
+  m_vertexWeight(vertexWeight, numVertices),
+  m_edgeWeight(edgeWeight, numEdges)
+{
+  ASSERT_NOTNULL(edgePrefix);
+
+  // calculate total vertex weight
+  if (m_numVertices > 0) {
+    if (m_vertexWeight.data() == nullptr) {
+      // allocate uniform vertex weight
+      m_vertexWeight = sl::Array<vtx_type>(m_numVertices, 1);
+      m_uniformVertexWeight = true;
+    } else {
+      const wgt_type baseVertexWeight = m_vertexWeight[0];
+      for (vtx_type v = 0; v < m_numVertices; ++v) {
+        const wgt_type vwgt = m_vertexWeight[v];
+        m_totalVertexWeight += vwgt;
+
+        if (vwgt != baseVertexWeight) {
+          m_uniformVertexWeight = false;
+        }
+      }
+    }
+  }
+
+  // calculate total edge weight
+  if (m_numEdges > 0) {
+    if (m_edgeWeight.data() == nullptr) {
+      // allocate uniform edge weight
+      m_edgeWeight = sl::Array<wgt_type>(m_numEdges, 1);
+      m_uniformEdgeWeight = true;
+    } else {
+      const wgt_type baseEdgeWeight = m_edgeWeight[0];
+      for (adj_type e = 0; e < m_numEdges; ++e) {
+        const wgt_type ewgt = m_edgeWeight[e];
+        m_totalEdgeWeight += ewgt;
+
+        if (ewgt != baseEdgeWeight) {
+          m_uniformEdgeWeight = false;
+        }
+      }
+    }
+  }
+}
+
+
+Graph::Graph(
+    Graph && lhs) noexcept :
+  m_uniformEdgeWeight(lhs.m_uniformEdgeWeight),
+  m_uniformVertexWeight(lhs.m_uniformVertexWeight),
+  m_numVertices(lhs.m_numVertices),
+  m_numEdges(lhs.m_numEdges),
+  m_totalVertexWeight(lhs.m_totalVertexWeight),
+  m_totalEdgeWeight(lhs.m_totalEdgeWeight),
+  m_edgePrefix(std::move(lhs.m_edgePrefix)),
+  m_edgeList(std::move(lhs.m_edgeList)),
+  m_vertexWeight(std::move(lhs.m_vertexWeight)),
+  m_edgeWeight(std::move(lhs.m_edgeWeight))
+{
+  // destrory old graph's data
+  lhs.m_numVertices = 0;
+  lhs.m_numEdges = 0;
+  lhs.m_totalVertexWeight = 0;
+  lhs.m_totalEdgeWeight = 0;
+}
+
+
+/******************************************************************************
+* PUBLIC METHODS **************************************************************
+******************************************************************************/
+
+#ifndef NDEBUG
+bool Graph::isValid() const
+{
+  // check vertex weight sum
+  wgt_type vertexSum = 0;
+  for (vtx_type i = 0; i < m_numVertices; ++i) {
+    vertexSum += m_vertexWeight[i];
+  }
+  if (vertexSum != m_totalVertexWeight) {
+    return false;
+  }
+
+  // check edge list
+  for (adj_type i = 0; i < m_numEdges; ++i) {
+    if (m_edgeList[i] >= m_numVertices) {
+      // invalid vertex
+      return false;
+    }
+  }
+
+  // check edge weight sum
+  wgt_type edgeSum = 0;
+  for (adj_type i = 0; i < m_numEdges; ++i) {
+    edgeSum += m_edgeWeight[i];
+  }
+  if (edgeSum != m_totalEdgeWeight) {
+    return false;
+  }
+
+  // check symmetry
+  for (vtx_type v = 0; v < m_numVertices; ++v) {
+    for (adj_type j = m_edgePrefix[v]; j < m_edgePrefix[v+1]; ++j) {
+      // find reverse edge
+      vtx_type const u = m_edgeList[j];
+      bool found = false;
+      for (adj_type k = m_edgePrefix[u]; k < m_edgePrefix[u+1]; ++k) {
+        if (v == m_edgeList[k]) {
+          if (m_edgeWeight[k] != m_edgeWeight[j]) {
+            return false;
+          }
+          found = true;
+          break;
+        }
+      }
+      if (!found) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+#endif
+
+}
